ee51f5c91800bf4c3e5b89ee68b2aa4c8983af0f
Sitam <sitammeur@gmail.com>
2024-02-27 19:01:58 +0530

    initial templates and static files setup

--- socials/settings.py
+++ socials/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "socials.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -136,7 +141,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 JAZZMIN_SETTINGS = {
     "site_header": "Socials",
     "site_brand": "Socials Admin",
-    #"site_logo": "images/logo.png",
+    # "site_logo": "images/logo.png",
     "copyright": "Socials ltd",
     "welcome_sign": "Welcome to Socials Admin, Login now !",
     "topmenu_links": [],
