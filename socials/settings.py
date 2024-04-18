ec9e04a6655d1f3f56d30b0a2d3df855c57986cc
Sitam <sitammeur@gmail.com>
2024-04-18 13:09:25 +0530

    abstract user model created

--- socials/settings.py
+++ socials/settings.py
@@ -138,6 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "userauths.User"
+
 JAZZMIN_SETTINGS = {
     "site_header": "Socials",
     "site_brand": "Socials Admin",
--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+GENDER = (("male", "Male"), ("female", "Female"))
+
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=200)
+    gender = models.CharField(max_length=100, choices=GENDER)
+
+    otp = models.CharField(max_length=10, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
