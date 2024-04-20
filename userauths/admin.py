f8238970359e3f8ef8f02c46d8f02ba96904bf47
Sitam <sitammeur@gmail.com>
2024-04-20 18:48:43 +0530

    admin settings updated

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from userauths.models import User, UserProfile
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "full_name", "email", "gender"]
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "full_name", "verified"]
+    list_editable = ["verified"]
+
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(UserProfile)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
